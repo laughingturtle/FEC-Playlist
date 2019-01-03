e4d1c8188a20f7fd6857a2ddd3782b34869a369c
apatoliya <apatoliya@gmail.com>
2019-01-03 13:07:04 -0600

    added seed db script

--- database/index.js
+++ database/index.js
@@ -1,25 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/audiodb');
+//mongoose.connect('mongodb://localhost/audiodb');
 
-let repoSchema = mongoose.Schema({
+let songSchema = mongoose.Schema({
     name: String,
     time: String,
     artist: String,
     album: String
 });
 
-let Repo = mongoose.model('Repo', repoSchema)
-let save = (name,time,artist,album) => {
-    let newRepo = new Repo({
-        name:name,
-        time:time,
-        artist:artist,
-        album:album
-    })
-
-    newRepo.save(function(err){
-        if(err){console.log('there is an error to store in DB')}
-        else {console.log('data store successfully in DB!!')}
-    });
-}
-module.exports.save = save
+let songs = mongoose.model('songs', songSchema)
+module.exports.songs = songs;
---
+++ database/seed.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/audiodbtest');
+//const songs = require('./index.js')
+let db = mongoose.connection;
+const faker = require('faker');
+
+let songSchema = mongoose.Schema({
+    name: String,
+    time: String,
+    artist: String,
+    album: String
+});
+let songs = mongoose.model('songs', songSchema)
+let songsCreate = () => {
+    let songsList = []
+    for(let i=0;i<50;i++){
+        const songEntry  = {
+            name: faker.name.findName(),
+            time: Math.floor(Math.random() * 10)+ 'min',
+            artist: faker.name.findName(),
+            album: faker.random.words()
+        }
+        songsList.push(songEntry)
+    }
+    return songsList;
+}
+
+const savedb = () =>{
+    songs.create(songsCreate())
+    .then(() => db.close());
+}
+savedb();
