95c83c96245c080207cf452880686d7e64f0bc3e
apatoliya <apatoliya@gmail.com>
2019-01-04 13:59:32 -0600

    added button and image filtering for songs

--- sampleData/exampleAudioData.js
+++ sampleData/exampleAudioData.js
@@ -1,20 +1,26 @@
- window.exampleAudioData = [
+  module.exports = [
     {
     name:"Bee Moved",
     time: "0:40",
     artist: "Blue Monday FM",
-    album: "Bee Moved!"
+    album: "Bee Moved!",
+    file_url: "//Users/songs/song1",
+    playlist_name: "Pop"
     },
     {
     name:"Bee Moved1",
     time: "0:40",
     artist: "Blue Monday FM1",
-    album: "Bee Moved!1"
+    album: "Bee Moved!1",
+    file_url: "//Users/songs/song2",
+    playlist_name: "Jazz"
     },
     {
-    name:"Bee Moved1",
+    name:"Bee Moved2",
     time: "0:40",
     artist: "Blue Monday FM1",
-    album: "Bee Moved!1"
+    album: "Bee Moved!1",
+    file_url: "//Users/songs/song3",
+    playlist_name: "Rock"
     }
 ];
