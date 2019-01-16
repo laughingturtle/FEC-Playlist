e56a9fdf2ad1873e48eea22a88ae311bb8dcb1fb
apatoliya <apatoliya@gmail.com>
2019-01-15 20:14:14 -0600

    modify the code to play actual songs

--- sampleData/exampleAudioData.js
+++ sampleData/exampleAudioData.js
@@ -1,34 +1,90 @@
   module.exports = [
     {
-    name:"Bee Moved",
-    time: "0:40",
-    artist: "Blue Monday FM",
-    album: "Bee Moved!",
-    file_url: "//Users/songs/song1",
+    name:"Dream",
+    time: "1:16",
+    artist: "Jared Horne",
+    album: "Dream Tour",
+    file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Dream.mp3",
     playlist_name: "Pop"
     },
     {
-    name:"Bee Moved1",
-    time: "0:40",
-    artist: "Blue Monday FM1",
-    album: "Bee Moved!1",
-    file_url: "//Users/songs/song2",
+    name:"epic",
+    time: "1:34",
+    artist: "Jared Horne",
+    album: "Dream Tour",
+    file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Epic.mp3",
     playlist_name: "Jazz"
     },
     {
-    name:"Bee Moved2",
-    time: "0:40",
-    artist: "Blue Monday FM1",
-    album: "Bee Moved!1",
-    file_url: "//Users/songs/song3",
+    name:"Float",
+    time: "1:17",
+    artist: "Jared Horne",
+    album: "Dream Tour",
+    file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Float.mp3",
     playlist_name: "Rock"
     },
     {
-    name:"Bee Moved3",
-    time: "0:30",
-    artist: "Blue Monday FM3",
-    album: "Bee Moved!3",
-    file_url: "//Users/songs/song4",
-    playlist_name: "Rock"
-    }
+      name:"GoodBye",
+      time: "1:11",
+      artist: "Jared Horne",
+      album: "Dream Tour",
+      file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Goodbye.mp3",
+      playlist_name: "Rock"
+      },
+      {
+        name:"Happy",
+        time: "2:21",
+        artist: "Jared Horne",
+        album: "Dream Tour",
+        file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Happy.mp3",
+        playlist_name: "Rock"
+        },
+        {
+          name:"Life",
+          time: "1:54",
+          artist: "Jared Horne",
+          album: "Dream Tour1",
+          file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Life.mp3",
+          playlist_name: "Jazz"
+          },
+          {
+            name:"Piece",
+            time: "1:44",
+            artist: "Jared Horne",
+            album: "Dream Tour1",
+            file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Peace.mp3",
+            playlist_name: "Rock"
+            },
+            {
+              name:"SciFi",
+              time: "1:03",
+              artist: "Jared Horne",
+              album: "Dream Tour1",
+              file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/SciFi.mp3",
+              playlist_name: "Pop"
+              },
+              {
+                name:"Sunshine",
+                time: "1:49",
+                artist: "Jared Horne",
+                album: "Dream Tour1",
+                file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Sunshine.mp3",
+                playlist_name: "Pop"
+                },
+                {
+                  name:"Triumph",
+                  time: "2:25",
+                  artist: "Jared Horne",
+                  album: "Dream Tour1",
+                  file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Triumph.mp3",
+                  playlist_name: "Rock"
+                  },
+                  {
+                    name:"Wonder",
+                    time: "0:46",
+                    artist: "Jared Horne",
+                    album: "Dream Tour1",
+                    file_url: "https://s3.us-east-2.amazonaws.com/rpt10-mondaymonday-songs/Wander.mp3",
+                    playlist_name: "Pop"
+                    },
 ];
